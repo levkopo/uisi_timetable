059bc9c51048fe4b500bc950bd0d8338e768c646
levkopo <levkopoag@gmail.com>
2021-10-26 09:37:57 +0500

    fix

--- main.js
+++ main.js
@@ -1,6 +1,11 @@
 const names = ["Сегодня", "Завтра", "Послезавтра"]
 let baseTable = null
 
+let src = '//cdn.jsdelivr.net/npm/eruda';
+if (!/eruda=true/.test(window.location) && localStorage.getItem('active-eruda') != 'true') return;
+document.write('<scr' + 'ipt src="' + src + '"></scr' + 'ipt>');
+document.write('<scr' + 'ipt>eruda.init();</scr' + 'ipt>');
+
 // VK Bridge Init
 if(typeof window['AndroidBridge'] != "undefined"){
     window['AndroidBridge']['VKWebAppInit']("{}")
